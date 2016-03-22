3849861aef9b793c9f7bd270566b8082dd6549a1
Ryan Schmukler <rschmukler@gmail.com>
2016-03-22 12:12:54 -0400

    initial implementation

---
+++ broadcast.go
@@ -0,0 +1,87 @@
+package broadcast
+
+import "sync"
+
+// A Broadcaster is used to broadcast messages to multiple listeners at once
+type Broadcaster struct {
+	listeners []chan interface{}
+	cap       int
+
+	mu sync.Mutex
+}
+
+// New builds a new generic Broadcaster. Cap is used as the buffer for each listener. If no cap
+// is specified, a capacity of 0 will be used
+func New(capArg ...int) *Broadcaster {
+	var cap int
+	if len(capArg) == 0 {
+		cap = 0
+	} else {
+		cap = capArg[0]
+	}
+	return &Broadcaster{
+		listeners: []chan interface{}{},
+		cap:       cap,
+		mu:        sync.Mutex{},
+	}
+}
+
+// Broadcast broadcasts a message to all listeners
+func (b *Broadcaster) Broadcast(data interface{}) {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	wg := sync.WaitGroup{}
+	wg.Add(len(b.listeners))
+
+	for _, listener := range b.listeners {
+		// Send off to listeners in parallel
+		go func(listener chan interface{}) {
+			listener <- data
+			wg.Done()
+		}(listener)
+	}
+
+	wg.Wait()
+}
+
+// Listen registers a new listener for the broadcast
+func (b *Broadcaster) Listen() <-chan interface{} {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	listener := make(chan interface{}, b.cap)
+	b.listeners = append(b.listeners, listener)
+
+	return listener
+}
+
+// Close closes all listener channels
+func (b *Broadcaster) Close() {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	for _, listener := range b.listeners {
+		close(listener)
+	}
+}
+
+// Reset closes all listener channels and removes all known listeners from the broadcaster
+func (b *Broadcaster) Reset() {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	for _, listener := range b.listeners {
+		close(listener)
+	}
+	b.listeners = []chan interface{}{}
+}
+
+// Remove removes a listener from the known listeners for a broadcaster
+func (b *Broadcaster) Remove(listener <-chan interface{}) {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	for i, targetListener := range b.listeners {
+		if targetListener == listener {
+			b.listeners = append(b.listeners[:i], b.listeners[i+1:]...)
+		}
+	}
+}
---
+++ broadcast_test.go
@@ -0,0 +1,63 @@
+package broadcast
+
+import (
+	"sync"
+	"testing"
+)
+
+func TestBroadcaster(t *testing.T) {
+	broadcaster := New()
+
+	var (
+		recvCount     int
+		listenerCount int
+		data          = 1
+	)
+
+	wg := sync.WaitGroup{}
+
+	addListener := func() {
+		listenerCount++
+		wg.Add(1)
+		recv := <-broadcaster.Listen()
+		if recv != data {
+			t.Fatalf("Received incorrect data.\n\tExpected: %v\n\tReceived: %v", data, recv)
+		}
+		recvCount++
+	}
+
+	go addListener()
+	go addListener()
+	wg.Wait()
+
+	if recvCount != listenerCount {
+		t.Fatal("Received incorrect number of events")
+	}
+
+	broadcaster.Reset()
+}
+
+func TestBroadcaster_Reset(t *testing.T) {
+	broadcaster := New()
+
+	broadcaster.Listen()
+	broadcaster.Listen()
+	broadcaster.Reset()
+
+	// Since capacity is 0, this would block if there were listeners
+	broadcaster.Broadcast(1)
+}
+
+func TestBroadcaster_Remove(t *testing.T) {
+	broadcaster := New()
+
+	listener := broadcaster.Listen()
+
+	go func() {
+		<-listener
+		t.Fatal("Listener received broadcast after being removed")
+	}()
+
+	broadcaster.Remove(listener)
+	broadcaster.Broadcast(1)
+}
